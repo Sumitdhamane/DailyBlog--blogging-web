00751de3d5114e39d5e08876e25a8484982ce446
Sumit Dhamane <sumitdhamane64@gmail.com>
2024-08-20 20:36:22 +0530

    connected to mongoDB

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+if (!process.env.MONGOOSE) {
+  console.error("MONGOOSE connection string is not defined in .env file.");
+  process.exit(1); // Exit the application
+}
+
+mongoose
+  .connect(process.env.MONGOOSE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running perfectly ");
+  console.log("Server is running perfectly");
 });
